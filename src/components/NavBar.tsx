604da2c2eacaf9c5e3db37c3571b72757cacc014
Yosmar Hinestroza <yosmarpc@gmail.com>
2025-01-04 13:24:45 -0300

    se agrega link de cada uno de los enlaces

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,15 +8,17 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav">
                     <li className="nav-item">
-                      <a className="nav-link active" aria-current="page" href="#">Repositorio</a>
+                      <a className="nav-link active" aria-current="page" href="https://github.com/yosmarcode/demo-table-component/tree/dev" target="_blank">Repositorio</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link active" aria-current="page" href="#">Documentación</a>
+                      <a className="nav-link active" aria-current="page" href="https://www.npmjs.com/package/table-component-mui-lib" target='_blank'>Documentación</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link active" aria-current="page" href="#">Video</a>
+                      <a className="nav-link active" aria-current="page" href="https://www.youtube.com/watch?v=Rb5vrl19yPc" target='_blank'>Video</a>
                     </li>
-                   
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="https://www.youtube.com/channel/UCSGaGrG-RpavXaJhJJQ_ktQ/?sub_confirmation=1" target='_blank'>suscribete @yosmarcode</a>
+                    </li>
                   </ul>
                 </div>
               </div>
